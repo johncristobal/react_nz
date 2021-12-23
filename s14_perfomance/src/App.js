a6b6dd3b0df5afa62c42d7c8001533eadc77ebf1
Cristobal <cristobaljohn00@gmail.com>
2021-12-23 16:43:27 -0600

    S14_perfomance

---
+++ s14_perfomance/src/App.js
@@ -0,0 +1,26 @@
+import { useState, useCallback, useMemo } from 'react'
+import Title from './components/Title'
+import MyForm from './components/Forms/MyForm'
+import MyList from './components/Lists/MyList'
+
+function App() {
+  const [valores, setValores] = useState([])
+  const handleSubmit = useCallback((values) => {
+    setValores(data => ([
+      ...data,
+      values
+    ]))
+  }, [
+
+  ])
+
+  return (
+    <div>
+      <Title>Mi título</Title>
+      <MyForm onSubmit={handleSubmit} />
+      <MyList data={valores} />
+    </div>
+  );
+}
+
+export default App;
